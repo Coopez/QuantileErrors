Fixed a number of bugs: Ace, Mae, no gradient for Linear, etc - see more in notion notes. Added function for metrics to be skipped every x epoch

layers/calibrated_linear.py
@@ -70,10 +70,10 @@ class Linear(ConstrainedModule):
         Raises:
             ValueError: If monotonicities does not have length input_dim (if provided).
         """
-        super().__init__()
+        super(Linear,self).__init__()
         self.output_dim = output_dim
         self.input_dim = input_dim
-        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        
         if monotonicities and len(monotonicities) != input_dim:
             raise ValueError("Monotonicities, if provided, must have length input_dim.")
         self.monotonicities = (
@@ -84,11 +84,11 @@ class Linear(ConstrainedModule):
         self.use_bias = use_bias if not weighted_average else False
         self.weighted_average = weighted_average
 
-        self.kernel = torch.nn.Parameter(torch.Tensor(input_dim, output_dim).double()).to(device)
+        self.kernel = torch.nn.Parameter(torch.Tensor(input_dim, output_dim).double(),requires_grad=True)
         torch.nn.init.constant_(self.kernel, 1.0 / input_dim)
         if use_bias:
-            self.bias = torch.nn.Parameter(torch.Tensor(1).double()).to(device)
-            torch.nn.init.constant_(self.bias, 0.0)
+            self.bias = torch.nn.Parameter(torch.Tensor([0.0]).double())
+            #torch.nn.init.constant_(self.bias, 0.0)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """Transforms inputs using a linear combination.
@@ -99,7 +99,7 @@ class Linear(ConstrainedModule):
         Returns:
             torch.Tensor of shape `(batch_size, 1)` containing transformed input values.
         """
-        result = torch.mm(x, self.kernel)
+        result = torch.matmul(x, self.kernel)
         if self.use_bias:
             result += self.bias
         return result

main.py
@@ -42,7 +42,7 @@ if _LOG_NEPTUNE:
     run['parameters'] = stringify_unsupported(params) # neptune only supports float and string
     
 train,train_target,valid,valid_target,_,_ = data_import()
-train = train[:,11]
+#train = train[:,11] # disable if training on all features/stations
 valid = valid[:,11]
 
 # normalize train and valid
@@ -88,9 +88,10 @@ lattice_paras = {
     'num_lattice_first_layer':_NUM_LATTICE_FIRST_LAYER,
     'output_size':params['_PRED_LENGTH'],
     'calibration_keypoints':params['_NUM_KEYPOINTS'],
+    'device':device
     }
 model = LSTM_Lattice(lstm_paras, lattice_paras,
-                     params=params)
+                     params=params).to(device)
 
 # Forward pass
 # Define loss function and optimizer
@@ -141,43 +142,46 @@ for epoch in range(epochs):
             if _LOG_NEPTUNE:
                 run['train/'+params['loss_option'][params['_LOSS']]].log(np.mean(train_losses))
     model.eval()
-    with torch.no_grad():
-        #valid_losses = []
-        metric_dict = {}
-        for batch in data_loader_valid:
+    metric_dict = {}
+    if epoch % params['_METRICS_EVERY_X'] == 0:
+        # Validation
+        with torch.no_grad():
+            #valid_losses = []
             
-            training_data, target = batch
-            quantile = data_valid.return_quantile(training_data.shape[0])
-            # Forward pass
-            output = model(training_data,quantile)
-            
-            # Compute loss
-              #criterion(output, target, quantile.unsqueeze(-1),
-                    #         type=params['loss_option'][params['_LOSS']])
-            #deb = Debug_model(model,training_data,quantile)
-            #deb.plot_out()
-            loss = metric(output, target.type(torch.double),input=training_data ,model=model,quantile=quantile.unsqueeze(-1),options = extra_options)
-            for key, value in loss.items():
-                # value is a tensor, we need to extract the value, but they may be a dict
-                # if isinstance(value, dict):
-                #     value = [v.item() for v in value.values()]
-                # else:
-                #     value = value.item()
+            for batch in data_loader_valid:
                 
-                if key in metric_dict:
-                    metric_dict[key].append(value)
+                training_data, target = batch
+                quantile = data_valid.return_quantile(training_data.shape[0])
+                # Forward pass
+                output = model(training_data,quantile)
+                
+                # Compute loss
+                #criterion(output, target, quantile.unsqueeze(-1),
+                        #         type=params['loss_option'][params['_LOSS']])
+                #deb = Debug_model(model,training_data,target)
+                #deb.plot_out()
+                loss = metric(output, target.type(torch.double),input=training_data ,model=model,quantile=quantile.unsqueeze(-1),options = extra_options)
+                for key, value in loss.items():
+                    # value is a tensor, we need to extract the value, but they may be a dict
+                    # if isinstance(value, dict):
+                    #     value = [v.item() for v in value.values()]
+                    # else:
+                    #     value = value.item()
+                    
+                    if key in metric_dict:
+                        metric_dict[key].append(value)
+                    else:
+                        metric_dict[key] = [value]
+                    #valid_losses.append(loss.item())
+        if _LOG_NEPTUNE:
+            #log all metrics in metric_dict to neptune
+            for key, value in metric_dict.items():
+                # test if value is a list 
+                if isinstance(value[0], np.ndarray): # need to check if in the list of batch accumulated values we have an array
+                        value = np.stack(value,axis=-1).mean(axis=-1).tolist()
+                        run['valid/'+key].log(stringify_unsupported(value))
                 else:
-                    metric_dict[key] = [value]
-                #valid_losses.append(loss.item())
-    if _LOG_NEPTUNE:
-        #log all metrics in metric_dict to neptune
-        for key, value in metric_dict.items():
-            # test if value is a list 
-            if isinstance(value[0], np.ndarray): # need to check if in the list of batch accumulated values we have an array
-                    value = np.stack(value,axis=-1).mean(axis=-1).tolist()
-                    run['valid/'+key].log(stringify_unsupported(value))
-            else:
-                run['valid/'+key].log(np.mean(value))
+                    run['valid/'+key].log(np.mean(value))
 
     
     epoch_time = time.time() - start_time

metrics/metrics.py
@@ -496,6 +496,7 @@ class Metrics():
         self.params = params
         self.lambda_ = params["_BEYOND_LAMBDA"] 
         self.batchsize = params["_BATCHSIZE"]
+        self.horizon = params["_PRED_LENGTH"]
         self.normalizer = normalizer 
     @torch.no_grad()
     def __call__(self, pred, truth,input=None,quantile=None,model=None, options={}):
@@ -503,8 +504,8 @@ class Metrics():
         pred_denorm = self.normalizer.inverse_transform(pred)
         truth_denorm = self.normalizer.inverse_transform(truth)
         if quantile is not None:
-
-            results['Pinball'] = pinball_loss(pred_denorm, truth_denorm, quantiles=quantile).item()
+            pinball = SQR_loss(type='pinball_loss',lambda_=self.lambda_)
+            results['Pinball'] = pinball(pred_denorm, truth_denorm, quantile=quantile).item()
             beyond_loss = SQR_loss(type='pinball_loss',lambda_=self.lambda_)
             results['Beyond'] = beyond_loss(pred_denorm, truth_denorm, quantile=quantile).item()
         for metric in self.metrics:
@@ -514,20 +515,20 @@ class Metrics():
 
             if metric == 'MAE':
                 mae = L1Loss()
-                results['MAE'] = mae(median,truth).item()#MAE(median, truth)
+                results['MAE'] = mae(median,truth_denorm).item()#MAE(median, truth)
             elif metric == 'MSE':
-                results['MSE'] = MSE(median, truth).item()
+                results['MSE'] = MSE(median, truth_denorm).item()
             elif metric == 'RMSE':
                 rmse = MSELoss()
-                results['RMSE'] =  torch.sqrt(rmse(median,truth)).item() #RMSE(median, truth)
+                results['RMSE'] =  torch.sqrt(rmse(median,truth_denorm)).item() #RMSE(median, truth)
             elif metric == 'MAPE':
-                results['MAPE'] = MAPE(median, truth).item()
+                results['MAPE'] = MAPE(median, truth_denorm).item()
             elif metric == 'MSPE':
-                results['MSPE'] = MSPE(median, truth).item()
+                results['MSPE'] = MSPE(median, truth_denorm).item()
             elif metric == 'RSE':
-                results['RSE'] = RSE(median, truth).item()
+                results['RSE'] = RSE(median, truth_denorm).item()
             elif metric == 'CORR':
-                results['CORR'] = CORR(median, truth).item()
+                results['CORR'] = CORR(median, truth_denorm).item()
             elif metric == 'PINAW':
                 results['PINAW'] = PINAW(cdf, truth,quantiles=q).item()
             elif metric == 'PICP':
@@ -535,7 +536,7 @@ class Metrics():
                 results["PICP"] = np.array([v.item() for v in value.values()])
             elif metric == 'ACE':
                 picp = PICP(cdf, truth,quantiles=q)
-                results['ACE'] = (ACE(picp)/self.batchsize).item()    
+                results['ACE'] = (ACE(picp)/(self.batchsize*self.horizon)).item()    
             elif metric == 'CRPS':
                 results['CRPS'] = eval_crps(cdf, truth, options).item()
             elif metric == 'Calibration':
@@ -546,7 +547,7 @@ class Metrics():
                 assert input is not None, "Input (X) is required for skill score's persistence model"
                 assert 'horizon' in options, "Horizon is required for skill score"
                 assert 'lookback' in options, "Lookback is required for skill score"
-                results['skill_score'] = Skill_score(options["horizon"],options["lookback"])(median, truth,input)
+                results['skill_score'] = Skill_score(options["horizon"],options["lookback"])(median, truth_denorm,input)
             else:
                 raise ValueError(f"Unknown metric: {metric}")
         return results

res/data.py
@@ -89,8 +89,8 @@ class Data_Normalizer():
     Always uses min-max normalization.
     """
     def __init__(self,
-                 train: np.ndarray = None,
-                 train_target: np.ndarray = None,
+                 train: np.ndarray,
+                 train_target: np.ndarray,
                  valid: np.ndarray = None,
                  valid_target: np.ndarray = None,
                  test: np.ndarray = None,
@@ -113,26 +113,25 @@ class Data_Normalizer():
         self.test = test
         self.test_target = test_target
         
-        if train is not None:
-            self.min_train = np.min(train)
-            self.max_train = np.max(train)
-        if train_target is not None:
-            self.min_train_target = np.min(train_target)
-            self.max_train_target = np.max(train_target)
-        if valid is not None:
-            self.min_valid = np.min(valid)
-            self.max_valid = np.max(valid)
-        if valid_target is not None:
-            self.min_valid_target = np.min(valid_target)
-            self.max_valid_target = np.max(valid_target)
-        if test is not None:
-            self.min_test = np.min(test)
-            self.max_test = np.max(test)
-        if test_target is not None:
-            self.min_test_target = np.min(test_target)
-            self.max_test_target = np.max(test_target)
-            
+        self.min_train = np.min(train,axis=0)
+        self.max_train = np.max(train,axis=0)
 
+        self.min_train_target = np.min(train_target,axis=0)
+        self.max_train_target = np.max(train_target,axis=0)
+
+        self.min_valid = self.min_train
+        self.max_valid = self.max_train
+
+        self.min_valid_target = self.min_train_target
+        self.max_valid_target = self.max_train_target
+
+        self.min_test = self.min_train
+        self.max_test = self.max_train
+
+        self.min_test_target = self.min_train_target
+        self.max_test_target = self.max_train_target
+
+        
     ## Helper functions, not supposed to be called directly
     def apply_minmax(self,var:str):
         return (self.__dict__[var] - self.__dict__[f"min_{var}"])/(self.__dict__[f"max_{var}"] - self.__dict__[f"min_{var}"])
@@ -206,7 +205,7 @@ class Data_Normalizer():
         else:
             raise ValueError("Target must be either train, train_target, valid, valid_target, test or test_target")
 
-    def inverse_transform(self, data, target: str="train"):
+    def inverse_transform(self, data, target: str="train_target"):
         """
         Apply inverse min-max normalization to a specific dataset.
 