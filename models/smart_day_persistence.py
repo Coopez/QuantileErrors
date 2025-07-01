67c50e01f3c6fb30ff5a35ae58cbb2d89fd4aa71
Niklas <niklas.erd@gmail.com>
2025-07-01 18:30:44 +0200

    redone sp

--- models/smart_day_persistence.py
+++ models/smart_day_persistence.py
@@ -2,8 +2,38 @@ import numpy as np
 import torch
 import pandas as pd
 import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from res.data import data_import, Data_Normalizer
 from data_provider.data_loader import return_cs
+
+class sPersistence_Forecast():
+    """
+    Class for smart persistence forecast. 
+    Just fetches from a precomputed pickle file
+    """
+    def __init__(self,normalizer,params):
+        self.dat = pd.read_pickle("models/sunpoint_smart_persistence.pkl")
+        self.index = self.dat.index
+        self.data = np.array(self.dat["Value"].tolist(),dtype=np.float32)
+        self.data = torch.tensor(self.data).float()
+        self.min = normalizer.min_target
+        self.max = normalizer.max_target
+        self.data_normalized = (self.data-self.min)/(self.max-self.min)
+
+    def forecast_raw(self,timestamp):
+        persistence = self.data[timestamp,:]
+        # persistence = self._rolling_mean(input_tensor=persistence)[:,self.target_summary-1::self.target_summary]
+        # persistence = persistence.view(-1, self.target_summary).mean(dim=-1, keepdim=True).view(-1,self.current_horizon)
+        return persistence
+    def forecast(self,timestamp):
+        """
+        Forecasts the smart persistence for a given timestamp.
+        """
+        persistence = self.data_normalized[timestamp,:]
+        # persistence = self._rolling_mean(input_tensor=persistence)[:,self.target_summary-1::self.target_summary]
+        # persistence = persistence.view(-1, self.data.shape[-1]).mean(dim=-1, keepdim=True).view(-1,self.data.shape[-1])
+        return persistence
 
 def smart_persistence(input,clearsky,output_size=36,datatype="numpy"):
     """
@@ -53,19 +83,23 @@ def generate_persistence_data(horizon_size=36,window_size=96):
     valid_index = i_series[len(test_target):len(test_target)+len(valid_target)]
     overall_time = index.values
     
-    ghi = test_target + valid_target + train_target
-    cs_ghi = cs_test + cs_valid + cs_train
+    ghi = np.concatenate([test_target, valid_target, train_target], axis=0)
+    cs_ghi = np.concatenate([cs_test ,cs_valid , cs_train],axis = 0)
+ 
 
     # make persistence data as forecast from this current timestamp
-    start = 0 + window_size
+    start = 0 #+ window_size
     end = len(overall_time) - (horizon_size + window_size)
     pers_list =list()
-    for idx,stamp in enumerate(overall_time[start:end]):
-        pers = smart_persistence(ghi[idx:idx+persistence_size],cs_ghi[idx+window_size:idx+window_size+horizon_size],output_size=horizon_size)
-        pers_list.append({"Time": stamp, "Value": pers})
+    for idx,stamp in enumerate(overall_time[start:end], start=start):
+        pers = smart_persistence(ghi[idx+(window_size-persistence_size):idx+window_size],cs_ghi[idx+ window_size:idx+horizon_size+ window_size],output_size=horizon_size)
+        pers_list.append({"idx": idx, "Time": stamp, "Value": pers})
     pers_df = pd.DataFrame(pers_list)
-    pers_df.set_index("Time",inplace=True)
+    pers_df.set_index("idx",inplace=True)
 
     # save to pickle
     pers_df.to_pickle("models/sunpoint_smart_persistence.pkl")
-    pass
+    return
+
+if __name__ == "__main__":
+    generate_persistence_data(horizon_size=36,window_size=96)
